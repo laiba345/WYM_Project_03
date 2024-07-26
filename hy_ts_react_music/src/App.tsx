b1136f3c867ed43939c0d7616361a53016de56a9
KK <2846016855@qq.com>
2024-07-26 23:18:02 +0800

    react-redux的使用

--- hy_ts_react_music/src/App.tsx
+++ hy_ts_react_music/src/App.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
-import { useRoutes } from 'react-router-dom'
+import React, { Suspense } from 'react'
+import { useRoutes, Link } from 'react-router-dom'
+import { useSelector, shallowEqual } from 'react-redux'
 import routes from './router'
 
 function App() {
-    return <div className="App">{useRoutes(routes)}</div>
+    /**
+     * useSelector是 React-Redux提供的一个 Hook
+     * 用于从 Redux store 中提取数据
+     * 它的作用是允许你在函数组件中访问 Redux store 的状态
+     * 并且在 store 状态发生变化时重新渲染组件
+     *
+     * unknow 类型上面不能进行任何操作；
+     * 可以直接先解构赋值操作出来也行；
+     */
+    const { count, message } = useSelector(
+        (state: any) => ({
+            count: state.counter.count,
+            message: state.counter.message
+        }),
+        shallowEqual
+    )
+    return (
+        <div className="App">
+            <div className="nav">
+                <Link to="/discover">发现音乐</Link>
+                <Link to="/mine">我的音乐</Link>
+                <Link to="/focus">关注</Link>
+                <Link to="/download">下载客户端</Link>
+            </div>
+            <h2>当前计数为：{count}</h2>
+            <h2>当前计数为：{message}</h2>
+            <Suspense>
+                <div className="main">{useRoutes(routes)}</div>
+            </Suspense>
+        </div>
+    )
 }
 
 export default App
--- hy_ts_react_music/src/store/module/counter.ts
+++ hy_ts_react_music/src/store/module/counter.ts
@@ -2,12 +2,15 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const counterSlice = createSlice({
     // 这里不需要写什么类型，会自动进行类型推导
-    name:'counter', 
-    initialState:{
-
-    }, 
+    name: 'counter',
+    // 不用写类型，会自动进行类型推导操作
+    initialState: {
+        count: 100,
+        message: '哈哈哈哈'
+    },
     // 必须写上reducers
-    reducers:{
-
-    }
+    reducers: {}
 })
+
+// 使用了一定要进行导出操作
+export default counterSlice.reducer
